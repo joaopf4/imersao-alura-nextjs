ebeaa6ce0d53d7e1a45ba99ec85e366b2f367a51
Joao Pedro <joaopfa@hotmail.com>
2021-01-27 20:57:12 -0300

    aula03

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,27 +1,35 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
-export const Button = styled.button`
-  margin-top: 12px;
-  margin-bottom: 12px;
-  padding: 12px 32px;
-  width: 100%;
-  border: 1px solid ${({ theme }) => theme.colors.contrastText};
+const Button = styled.button`
   background-color: ${({ theme }) => theme.colors.primary};
   color: ${({ theme }) => theme.colors.contrastText};
-  border-radius: 4px;
+  border-radius: ${({ theme }) => theme.colors.borderRadius};
+  border: 0;
+  padding: 12px 16px;
+  width: 100%;
+  text-transform: uppercase;
+  line-height: 1;
+  font-size: 14px;
+  font-weight: bold;
+  outline:0;
   overflow: hidden;
-  h4 {
-    font-weight: 700;
-    line-height: 1;
-    margin: 0;
-  }
-  :hover{
-    background-color: ${({ theme }) => theme.colors.secondary};
-    cursor: pointer;
-    color: ${({ theme }) => theme.colors.primary};
-  }
-  :active{
+  cursor: pointer;
+  transition: .3s;
+
+  &:hover,
+  &:focus {
     background-color: ${({ theme }) => theme.colors.success};
   }
+  &:disabled {
+    background-color: #979797;
+    cursor: not-allowed;
+  }
 `;
+Button.propTypes = {
+  type: PropTypes.oneOf(['submit', 'type', 'button']).isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Button;
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
-// eslint-disable-next-line import/prefer-default-export
-export const Widget = styled.div`
+const Widget = styled.div`
   margin-top: 24px;
   margin-bottom: 24px;
   border: 1px solid ${({ theme }) => theme.colors.secondary};
@@ -43,3 +42,23 @@ Widget.Content = styled.div`
     padding: 0;
   }
 `;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}40`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: .3s;
+  display: block;
+  
+  &:hover,
+  &:focus {
+    opacity: .5;
+  }
+`;
+
+export default Widget;
