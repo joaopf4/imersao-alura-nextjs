b2fcd62a0f45ebc378c1ce02db275893b1a235c2
Joao Pedro <joaopfa@hotmail.com>
2021-02-02 13:19:30 -0300

    adding favicon

--- pages/_app.js
+++ pages/_app.js
@@ -38,8 +38,8 @@ export default function App({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
         <title>Cycling Quiz</title>
-        <link rel="shortcut icon" href="../public/faviconcycle.ico" />
-        <link rel="icon" href="%PUBLIC_URL%/faviconcycle.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
