6802b77c4c85b0c68b1758f92f960bd8ed7b0461
Joao Pedro <joaopfa@hotmail.com>
2021-02-02 15:35:25 -0300

    correcting favicon and some metatags

--- pages/_app.js
+++ pages/_app.js
@@ -37,14 +37,29 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
-        <title>Cycling Quiz</title>
-        <link rel="shortcut icon" href="/favicon.ico" />
 
+        <title>Cycling Quiz</title>
+        <link rel="shortcut icon" href="/favcycle.ico" />
+        <meta name="title" content="Cycling Quiz" />
+        <meta name="description" content="Um quiz sobre bicicletas pra ver o quanto você é pedaluda ou pedaludo!" />
+
+        {/* Open Graph / Facebook */}
         <meta name="viewport" />
         <meta property="og:image" content={db.bg} />
         <meta property="og:image:type" content="image/jpg" />
         <meta property="og:image:width" content="800" />
         <meta property="og:image:height" content="600" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Cycling Quiz" />
+        <meta property="og:description" content="Um quiz sobre bicicletas pra ver o quanto você é pedaluda ou pedaludo!" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Cycling Quiz" />
+        <meta property="twitter:description" content="Um quiz sobre bicicletas pra ver o quanto você é pedaluda ou pedaludo!" />
+        <meta property="twitter:image" content={db.bg} />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
